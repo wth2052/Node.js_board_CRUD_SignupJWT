748894cc19b91c602b4a6a4acbc5ba6ed61b97bf
“Woo <wth2052@gmail.com>
2022-12-21 19:45:01 +0900

    조회/전체조회/수정 update commit

--- routes/posts.js
+++ routes/posts.js
@@ -11,7 +11,7 @@ const authMiddleware = require("../middlewares/auth-middleware");
     const {user, password, title, content} = req.body
     //빈칸에 대한 if 요청, try catch 사용
       try {
-          await Posts.create({user, password, title, content})
+          await Post.create({user, password, title, content})
           return res.status(201).send({success: true,Message: '게시물 작성에 성공했습니다.'})
       } catch (err){ 
         if(!user || !password || !title || !content){
@@ -31,45 +31,47 @@ router.get("/", async (req, res) => {
 
 
 //GET 게시글 상세조회 완성
-router.get("/:postsId", async (req, res) => {
-  const { postsId } = req.params;
-  const getOnePosts = await Posts.find({ postsId: Number(postsId) });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const getOnePosts = await Post.findOne({
+    where: {id: id}
+  })
   res.status(200).json(getOnePosts);
 })
 
 
 
-//PUT 게시글 수정 완성
+//PUT 게시글 수정 
 // 발생할수 있는 상황 : 비밀번호 불일치, 비밀번호 입력안함, 비밀번호 일치
-router.put("/:postsId/", authMiddleware, async (req, res) => {
-  const { postsId } = req.params;
+router.put("/:id/", async (req, res) => {
+  const { id } = req.params;
   const { password, title, content } = req.body;
   // find 메소드 : 제공되는 조건을 만족하는 첫번째 엘리먼트 값을 리턴하는 함수
   // map함수는 callbackFunction을 실행한 결과를 가지고 새로운 배열을 만들 때 사용한다.
   // map 함수는 파라미터가 하나만 있을 때는 주변 괄호를 생략할 수 있다.
   //이때 posts 변수는 동기적으로 Posts에 있는 스키마를 토대로 자료를 검색함
   //const Posts = require("../schemas/posts.js")
-  const posts = await Posts.find();
-  //하나는 postsId를 하나는 password를 쭉 돌아 나온 결과를 새로운 배열로써 반환해줌.
-  const postsIds = posts.map((post) => post.postsId); 
+  const posts = await Post.findAll();
+  //하나는 id를 하나는 password를 쭉 돌아 나온 결과를 새로운 배열로써 반환해줌.
+  const ids = posts.map((post) => post.id); 
   // [1, 2, 3, 4, 5, 6, 7]
   const postsPws = posts.map((post) => post.password);
   // ['123456', '123456', '123456', '123456123', '123456123', '123456123', '123456123']
-  console.log(postsIds, postsPws)
-  // for문으로 postids 갯수만큼 도는데
-  // postsIds의 i번째 값과 postsId (api 주소에 있는 게시글 번호)와 비교한다.(주소창에 3을 입력시 i는 1 2 3 돌아 도착)
-  for(let i=0; i< postsIds.length; i++){
-    // 이때 postIds[i](for문의 i번째 postsId값)과 req.body에 담긴 postsId의 값과 일치하고
+  console.log(ids, postsPws)
+  // for문으로 ids 갯수만큼 도는데
+  // ids의 i번째 값과 id (api 주소에 있는 게시글 번호)와 비교한다.(주소창에 3을 입력시 i는 1 2 3 돌아 도착)
+  for(let i=0; i< ids.length; i++){
+    // 이때 postIds[i](for문의 i번째 id값)과 req.body에 담긴 id의 값과 일치하고
     // postsPws[i](for문의 i번째 password값)과 req.body에 담긴 password값과 일치할 경우
-    if(postsIds[i] === Number(postsId) && postsPws[i] === password) {
-      //Posts에 있는 자료를 postsId를 토대로 title contet값을 입력한 값으로 바꿔준다. 이때 작업은 동기적으로 처리한다.
-      await Posts.updateOne({ postsId: Number(postsId)}, { $set: {title, content} })
-      // console.log(postsIds[i],Number(postsId),postsPws[i],password)
+    if(ids[i] === Number(id) && postsPws[i] === password) {
+      //Posts에 있는 자료를 id를 토대로 title content값을 입력한 값으로 바꿔준다. 이때 작업은 동기적으로 처리한다.
+      await Post.update({title: title, content: content}, {where:{id}})
+      // console.log(ids[i],Number(id),postsPws[i],password)
       //값을 바꾼뒤 json으로 success true와 수정에 성공하였다는 응답을 반환한다.
       res.json({ success: true, Message: '수정에 성공하였습니다.' });
       //비밀번호가 일치하지 않을 시에 대한 처리
-      //postsId[i]와 postsId는 맞지만, postsPws[i]와 password가 일치하지 않을 경우
-    } else if (postsIds[i] === Number(postsId) && postsPws[i] != password){ 
+      //id[i]와 id는 맞지만, postsPws[i]와 password가 일치하지 않을 경우
+    } else if (ids[i] === Number(id) && postsPws[i] != password){ 
       //success false를 반환하고, 비밀번호가 일치하지 않는다는 응답을 반환한다.
       return res.json({ success: false, Message: '비밀번호가 일치하지 않습니다.' });
       //비밀번호를 입력하지 않을 시에 대한 처리
@@ -82,28 +84,28 @@ router.put("/:postsId/", authMiddleware, async (req, res) => {
 })
 
 //DELETE 삭제 함수 완성
-router.delete("/:postsId/", authMiddleware,async (req, res) => {
-  const { postsId } = req.params;
+router.delete("/:id/", authMiddleware,async (req, res) => {
+  const { id } = req.params;
   const { password } = req.body;
     //const Posts = require("../schemas/posts.js")
     const posts = await Posts.find();
-    //하나는 postsId를 하나는 password를 쭉 돌아 나온 결과를 새로운 배열로써 반환해줌.
-    const postsIds = posts.map((post) => post.postsId); 
+    //하나는 id를 하나는 password를 쭉 돌아 나온 결과를 새로운 배열로써 반환해줌.
+    const ids = posts.map((post) => post.id); 
     // [1, 2, 3, 4, 5, 6, 7]
     const postsPws = posts.map((post) => post.password);
     // ['123456', '123456', '123456', '123456123', '123456123', '123456123', '123456123']
   // for문으로 postids 갯수만큼 도는데
-  // postsIds의 i번째 값과 postsId (api 주소에 있는 게시글 번호)와 비교한다.(3을 입력시 i는 1 2 3 돌아 도착)
-  for(let i=0; i< postsIds.length; i++){
-    // 이때 postIds[i](for문의 i번째 postsId값)과 req.body에 담긴 postsId의 값과 일치하고
+  // ids의 i번째 값과 id (api 주소에 있는 게시글 번호)와 비교한다.(3을 입력시 i는 1 2 3 돌아 도착)
+  for(let i=0; i< ids.length; i++){
+    // 이때 postIds[i](for문의 i번째 id값)과 req.body에 담긴 id의 값과 일치하고
     // postsPws[i](for문의 i번째 password값)과 req.body에 담긴 password값과 일치할 경우
-    if(postsIds[i] === Number(postsId) && postsPws[i] === password) {
-      //Posts에 있는 자료를 postsId를 토대로 title contet값을 입력한 값으로 바꿔준다. 이때 작업은 동기적으로 처리한다.
-    await Posts.deleteOne({ postsId });
+    if(ids[i] === Number(id) && postsPws[i] === password) {
+      //Posts에 있는 자료를 id를 토대로 title contet값을 입력한 값으로 바꿔준다. 이때 작업은 동기적으로 처리한다.
+    await Posts.deleteOne({ id });
       res.json({ result: "success", Message: '삭제에 성공하였습니다.' });
       //비밀번호가 일치하지 않을 시에 대한 처리
-      //postsId[i]와 postsId는 맞지만, postsPws[i]와 password가 일치하지 않을 경우
-    } else if (postsIds[i] === Number(postsId) && postsPws[i] != password){ 
+      //id[i]와 id는 맞지만, postsPws[i]와 password가 일치하지 않을 경우
+    } else if (ids[i] === Number(id) && postsPws[i] != password){ 
       //success false를 반환하고, 비밀번호가 일치하지 않는다는 응답을 반환한다.
       return res.json({ success: false, Message: '비밀번호가 일치하지 않습니다.' });
       //비밀번호를 입력하지 않을 시에 대한 처리
